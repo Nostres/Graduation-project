4a54230c0e9faac06b0d58a14d8f26f497774f06
i.borsuk <i.borsuk@aimprosoft.com>
2017-05-12 15:05:57 +0300

    add particle autocorrelation

--- server/grails-app/services/graduation/CorrelationService.groovy
+++ server/grails-app/services/graduation/CorrelationService.groovy
@@ -5,6 +5,33 @@ import grails.transaction.Transactional
 @Transactional
 class CorrelationService {
 
+    private static List<Double> getAbscissa(def PACL, Integer size) {
+        Double [][] multidimensionalArray = PACL
+        List<Double> abscissa = []
+        for (int i = 0; i < size; i++) {
+            abscissa.add(multidimensionalArray[i][i])
+        }
+        return abscissa
+    }
+
+    private static Double countLL(List<Double> previousPACL, List<Double> sample, Integer l) {
+        Double numeratorSum = 0
+        for (int i = 0; i < l - 1; i++) {
+            numeratorSum += previousPACL.get(i + 1) * sample.get(l - i)
+        }
+        Double denominatorSum = 0
+        for (int i = 0; i < l - 1; i++) {
+            denominatorSum += previousPACL.get(i + 1) * sample.get(i)
+        }
+        Double numerator = sample.get(l) - numeratorSum
+        Double denominator = 1 - denominatorSum
+        return numerator / denominator
+    }
+
+    private static Double countLJ(List<Double> previousPACL, List<Double> currentPACL, Integer l, Integer j) {
+        return (previousPACL.get(j) - currentPACL.get(l) * previousPACL.get(l - j))
+    }
+
     static List<Double> autoCovariance(List<Double> sample, Double averageValue) {
         Long size = sample.size()
         List<Double> result = []
@@ -13,7 +40,7 @@ class CorrelationService {
             for (int t = 0; t < size - k - 1; t++) {
                 ac += ((sample.get(t) - averageValue) * (sample.get(t + k) - averageValue))
             }
-            result.add((ac / 730))
+            result.add((ac / size))
         }
         return result
     }
@@ -22,20 +49,25 @@ class CorrelationService {
         return autoCovarianceList.collect({ it / deviance })
     }
 
+    @SuppressWarnings("All")
     static List<Double> partialAutoCorrelation(List<Double> sample) {
-        Long size = sample.size()
-        Double [][] PACL = new Double[size][size]
+        Integer size = sample.size()
+        Double[][] PACL = new Double[size][size]
 
         //initial data
-        PACL[0][0] = sample.get(0)
-        PACL[1][1] = (sample.get(1) - Math.pow(sample.get(0), 2)) * (1 - Math.pow(sample.get(0), 2))
-        PACL[1][0] = (PACL[0][0] - PACL[1][1] * PACL[0][0])
+        PACL[1][1] = sample.get(0)
+        PACL[2][2] = (sample.get(1) - Math.pow(sample.get(0), 2)) * (1 - Math.pow(sample.get(0), 2))
+        PACL[2][1] = (PACL[1][1] - PACL[2][2] * PACL[1][1])
 
-        for (int l = 2; l < size; l++) {
+        for (int l = 3; l < size; l++) {
+            PACL[l][l] = countLL(PACL[l - 1] as List<Double>, sample, l as Integer)
 
+            for (int j = l - 1; j > 0; j--) {
+                PACL[l][j] = countLJ(PACL[l - 1] as List<Double>, PACL[l] as List<Double>, l, j)
+            }
         }
 
-
-        return []
+        return getAbscissa(PACL, size)
     }
+
 }
