913b34a1cfc11265acfaa2bab6712b9152eb8ea9
Sergei Yeryomin <s.yeryomin@aimprosoft.com>
2017-05-22 17:19:31 +0300

    added run python math server script

---
+++ mathserver/run.sh
@@ -0,0 +1,33 @@
+##!/bin/bash
+
+echo ""
+echo ""
+echo "Creating virtual environment..."
+virtualenv math
+echo "Ok..."
+
+echo ""
+echo ""
+echo "Activating virtual environment..."
+cd math
+source bin/activate
+echo "Ok..."
+
+echo ""
+echo ""
+echo "Installing dependencies..."
+pip install -r ./requirements.txt
+echo "Ok..."
+
+echo ""
+echo ""
+echo "Running python math server..."
+python src/graduation/math.py
+echo "Ok..."
+
+echo ""
+echo ""
+echo "Deactivating virtual environment..."
+deactivate
+echo "Ok..."
+echo "Bye!..."
